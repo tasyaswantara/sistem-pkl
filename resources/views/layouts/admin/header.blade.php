dddf68a6f8e60f981a038ef13c2660416e6b9b8f
tasyaswantara <tasyaswantara@gmail.com>
2026-01-20 18:14:35 +0700

    fix: header

--- resources/views/layouts/admin/header.blade.php
+++ resources/views/layouts/admin/header.blade.php
@@ -72,21 +72,21 @@
                 {{-- <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Layer_2" data-name="Layer 2"> <g id="invisible_box" data-name="invisible box"> <rect width="48" height="48" fill="none"></rect> </g> <g id="Layer_7" data-name="Layer 7"> <g> <path d="M25.1,41H4a2,2,0,0,1-2-2V31.1l1-.6A25.6,25.6,0,0,1,16,27a26.7,26.7,0,0,1,7.5,1.1,21.2,21.2,0,0,0-.5,4.4A18.4,18.4,0,0,0,25.1,41Z"></path> <path d="M16,23a9,9,0,1,0-9-9A9,9,0,0,0,16,23Z"></path> <path d="M46,34.1V31.9L42.4,31l-.5-1.1,2-3.2-1.6-1.6-3.2,2L38,26.6,37.1,23H34.9L34,26.6l-1.1.5-3.2-2-1.6,1.6,2,3.2L29.6,31l-3.6.9v2.2l3.6.9.5,1.1-2,3.2,1.6,1.6,3.2-2,1.1.5.9,3.6h2.2l.9-3.6,1.1-.5,3.2,2,1.6-1.6-2-3.2.5-1.1ZM36,36a3,3,0,1,1,3-3A2.9,2.9,0,0,1,36,36Z"></path> </g> </g> </g> </g></svg> --}}
                 {{-- <div>{{ Auth::user()->name }}
         </div> --}}
-                <span>{{ Auth::user()->roles->first()->name ?? 'user' }}</span>
-                <svg class="w-4 text-gray-400" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-                </svg>
-            </button>
+        <span>{{ Auth::user()->roles->first()->name ?? 'user' }}</span>
+        <svg class="w-4 text-gray-400" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+        </svg>
+        </button>
 
-            <div x-cloak x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 z-10 w-full h-full"></div>
+        <div x-cloak x-show="dropdownOpen" @click="dropdownOpen = false" class="fixed inset-0 z-10 w-full h-full"></div>
 
-            <div x-cloak x-show="dropdownOpen" class="absolute right-0 z-10 w-56 mt-2 overflow-hidden bg-white rounded-md shadow-xl border border-gray-200">
-                <div class="px-4 py-3">
-                    <div class="text-sm font-semibold text-gray-900">{{ Auth::user()->name }}</div>
-                    <div class="text-xs text-gray-500">{{ Auth::user()->roles->first()->name ?? 'user' }}</div>
-                </div>
+        <div x-cloak x-show="dropdownOpen" class="absolute right-0 z-10 w-56 mt-2 overflow-hidden bg-white rounded-md shadow-xl border border-gray-200">
+            <div class="px-4 py-3">
+                <div class="text-sm font-semibold text-gray-900">{{ Auth::user()->name }}</div>
+                <div class="text-xs text-gray-500">{{ Auth::user()->email }}</div>
             </div>
         </div>
+    </div>
 
     </div>
 </header>
